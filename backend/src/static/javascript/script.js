bc26d3e59f66993a7f08447ab7b4ba786c438db0
Enzo Agustín Fuentes <enzofuentes@dom.tdf.gob.ar>
2024-03-01 14:20:32 -0300

    cambia el color de los tickets segun el estado del ticket (naranja, amarillo o verde)

--- backend/src/static/javascript/script.js
+++ backend/src/static/javascript/script.js
@@ -6,10 +6,48 @@ var nuevo_ticket_btn = document.getElementById("nuevo_ticket_btn");
 var aceptar_nuevo_ticket_btn = document.getElementById("aceptar_nuevo_ticket_btn");
 var cancelar_nuevo_ticket_btn = document.getElementById("cancelar_nuevo_ticket_btn");
 
+// ticket row es como una coleccion de elementos asi que hay que loopearlos
+var ticket_rows = document.getElementsByClassName("ticket_row");
+
+for (var i = 0; i < ticket_rows.length; i++) {
+    ticket_rows[i].addEventListener("mouseover", function(event) {
+        // afecto al parent del target
+        //event.target.parentElement.style.backgroundColor = "tomato";
+        
+    });
+}
+
+for (var i = 0; i < ticket_rows.length; i++) {
+    ticket_rows[i].addEventListener("mouseout", function(event) {
+        // afecto al parent del target
+        //event.target.parentElement.style.backgroundColor = "white";
+        
+    });
+}
+
+for (var i = 0; i < ticket_rows.length; i++) {
+    //alert(ticket_rows[i].textContent[1])
+    var columnText = ticket_rows[i].getElementsByTagName("td")[8].textContent;
+    if (columnText == "Pendiente"){
+        ticket_rows[i].style.backgroundColor = "orange";
+    }
+
+    if (columnText == "Verificar"){
+        ticket_rows[i].style.backgroundColor = "yellow";
+    }
+
+    if (columnText == "Resuelto"){
+        ticket_rows[i].style.backgroundColor = "green";
+    }
+    
+}
+
+
 //muestro al hacer click
 nuevo_ticket_btn.onclick = function() {
     modal.style.display = "block";
   }
+
 //escondo al cancelar
 cancelar_nuevo_ticket_btn.onclick = function() {
     modal.style.display = "none";
@@ -25,6 +63,7 @@ aceptar_nuevo_ticket_btn.onclick = function(e) {
         observaciones: $("#observaciones").val(),
         csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
     };
+
     // configuro ajax
     $.ajax({
         type: 'POST',
